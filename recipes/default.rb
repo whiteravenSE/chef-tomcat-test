048d5c31d259269bf9ef2e140675ccc375f23edc
whiteravenSE <whiteravenSE@users.noreply.github.com>
2018-02-01 13:07:25 -0600

    Add files via upload

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-package 'java-1.7.0-openjdk-devel'
+#execute 'add-apt-repository ppa:openjdk-r/ppa'
+apt_repository 'openjdk' do
+  uri 'ppa:openjdk-r/ppa'
+end
+
+apt_update
+
+package 'openjdk-7-jre'
 
 group 'tomcat' 
 
@@ -22,20 +29,49 @@ end
 directory '/opt/tomcat' do
   owner 'tomcat'
   group 'tomcat'
-  recursive 'true'
+  recursive true
   action :create
 end
 
-execute 'tar xvf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1'
+execute 'tar xvf /tmp/apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1'
+
+directory '/opt/tomcat/conf' do
+  mode '0040'
+  recursive true
+end
+
+execute 'sudo chmod -R g+r /opt/tomcat/conf'
 
 directory '/opt/tomcat/conf' do
   mode '0050'
 end
+
+directory '/opt/tomcat' do
+  group 'tomcat'
+  recursive true
+end
+
+%w[ webapps work temp logs ].each do |path|
+  directory path do
+    owner 'tomcat'
+    recursive true
+  end
+end
+
+execute 'chgrp -R tomcat /opt/tomcat'
+
+execute 'chown -R tomcat /opt/tomcat/webapps/ /opt/tomcat/work/ /opt/tomcat/temp/ /opt/tomcat/logs/'
 
 template '/etc/systemd/system/tomcat.service' do
   source 'tomcat.service.erb'
 end
 
 execute 'systemctl daemon-reload'
-execute 'systemctl start tomcat'
-execute 'systemctl enable tomcat'
+#execute 'systemctl start tomcat'
+#execute 'systemctl enable tomcat'
+
+service 'tomcat' do
+  action [:start, :enable]
+end
+
+execute 'curl http://localhost:8080'
